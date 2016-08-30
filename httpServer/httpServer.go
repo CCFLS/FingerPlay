a55ffae71413a8a47fb84f8a1abd8debc90dff21
laiqifan <laiqifan@creditease.cn>
2016-08-30 18:03:51 +0800

    初步优化代码结构 增加锁(有效性待验证)

--- httpServer/httpServer.go
+++ httpServer/httpServer.go
@@ -5,6 +5,9 @@ import (
 	"encoding/json"
 	"github.com/golibs/uuid"
 	"io"
+	"sync"
+	"fmt"
+	"strconv"
 )
 
 var player map[string]string = make(map[string]string)
@@ -28,6 +31,8 @@ type initUserStatusParams struct {
 	Type string `json:"type"`
 }
 
+var lock sync.RWMutex
+
 func initUserStatus(w http.ResponseWriter, req *http.Request)  {
 	req.ParseForm()
 	parms := req.Form["params"][0]
@@ -36,46 +41,46 @@ func initUserStatus(w http.ResponseWriter, req *http.Request)  {
 	userName := initParams.UserName
 	Type := initParams.Type
 	uuid := uuid.Rand().Hex()
-	if Type == "1"{
-		watcher[userName]=uuid
-		initReturn := &initUserReturn{}
-		initReturn.Role="1"
-		initReturn.UserName=userName
-		initReturn.UserToken=uuid
-		returnJson := &ReturnJson{}
-		returnJson.Msg="请求成功"
-		returnJson.Error="0"
-		returnJson.Data=initReturn
-		jsonStr,_:=json.Marshal(returnJson)
-		io.WriteString(w,string(jsonStr))
-	}else if Type == "0"{
-		if len(player)<2{
-			player[userName]=uuid
-			initReturn := &initUserReturn{}
-			initReturn.Role="0"
-			initReturn.UserHP="10"
-			initReturn.UserName=userName
-			initReturn.UserToken=uuid
-			returnJson := &ReturnJson{}
-			returnJson.Msg="请求成功"
-			returnJson.Error="0"
-			returnJson.Data=initReturn
-			jsonStr,_:=json.Marshal(returnJson)
-			io.WriteString(w,string(jsonStr))
-		}else {
-			watcher[userName]=uuid
-			initReturn := &initUserReturn{}
-			initReturn.Role="1"
-			initReturn.UserName=userName
-			initReturn.UserToken=uuid
-			returnJson := &ReturnJson{}
-			returnJson.Msg="请求成功"
-			returnJson.Error="0"
-			returnJson.Data=initReturn
-			jsonStr,_:=json.Marshal(returnJson)
-			io.WriteString(w,string(jsonStr))
+	if Type =="0"{
+		size := len(player) //查看战局人数
+		fmt.Println("当前战局人数战局人数:"+strconv.Itoa(len(player)))
+		if size >=2 {
+			if _, ok := player[userName]; !ok {
+				//当前战局人数大于2且该用户不在战局内 则只能加入观战模式
+				Type ="1"
+			}
 		}
 	}
+	lock.Lock()
+	jsonStr := generatorUser(userName,uuid,Type)
+	lock.Unlock()
+	io.WriteString(w,string(jsonStr))
+}
+func generatorUser(userName string,uuid string,Type string) string {
+	initReturn := &initUserReturn{}
+	initReturn.UserName=userName
+	if Type == "0"{//参战
+		if _, ok := player[userName]; !ok {
+			//该用户不存在
+			player[userName]=uuid
+		}
+		initReturn.UserToken=player[userName]
+		initReturn.Role="0"
+		initReturn.UserHP="10"
+	}else if Type == "1"{//观战
+		if _, ok := watcher[userName]; !ok {
+			//该用户不存在
+			watcher[userName]=uuid
+		}
+		initReturn.UserToken=watcher[userName]
+		initReturn.Role="1"
+	}
+	returnJson := &ReturnJson{}
+	returnJson.Msg="请求成功"
+	returnJson.Error="0"
+	returnJson.Data=initReturn
+	jsonStr,_:=json.Marshal(returnJson)
+	return string(jsonStr)
 }
 
 func HttpServerStart()  {
